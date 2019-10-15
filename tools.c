f2de7c59544a2ad1e7108d9c95a9a76ae4275cbf
RitaMartinho <56005870+RitaMartinho@users.noreply.github.com>
2019-10-15 23:33:47 +0100

    Add files via upload

--- tools.c
+++ tools.c
@@ -83,7 +83,7 @@ int resetPort(int fd, struct termios *oldtio) {
 } 
 
 
-void buildConnectionFrame( char *connectionFrame, unsigned char A, unsigned char C){
+void buildConnectionFrame( char *connectionFrame, unsigned char A, unsigned char C){ // belongs to DATALINK
 
 	connectionFrame[0] = FLAG;
 	connectionFrame[1] = A;
@@ -94,7 +94,7 @@ void buildConnectionFrame( char *connectionFrame, unsigned char A, unsigned char
 } //supervisionFrame()
 
 
-void buildFrame( char * frame, int C_ns, char* message, int lenght){
+void buildFrame( char * frame, int C_ns, char* message, int lenght){ //belongs to DATALINK
 
 	
 	frame[0]=FLAG;
@@ -115,7 +115,7 @@ void buildFrame( char * frame, int C_ns, char* message, int lenght){
 }
 
 
-char buildBBC2(char *message, int lenght){
+char buildBBC2(char *message, int lenght){ //belongs to datalink
 
 	char BCC2=0;
 
@@ -125,4 +125,157 @@ char buildBBC2(char *message, int lenght){
 	}
 
 	return BCC2;
+}
+
+//builds data package from file
+int buildDataPackage(unsigned char* buffer, unsigned char* package, int size, int * seq_n){
+
+	int aux=0, i=0;
+	package[0]= AP_DATA; //C
+	package[1]=(char)(*seq_n)++;
+
+	if((*seq_n)== 256){ //module 255
+		*seq_n=0;
+	}
+
+	aux= size %256; //
+	package[2]=(size- aux)/256;
+	package[3]=aux;
+
+	for(i=0; i<size; i++){
+
+		package[i+4]=buffer[i]; // data read from file into application package
+	}
+
+	return i+4; // returns size of package
+}
+
+//rebuild packet from datalink into DataPackage specific struct
+void rebuildDataPackage(unsigned char* packet, DataPackage *packet_data){
+
+	int i=0, j=0;
+	int size_of_data=0;
+
+	(*packet_data).N = packet[0];
+	(*packet_data).L1= packet[1];
+	(*packet_data).L2= packet[2];
+	(*packet_data).file_data=(unsigned char*)malloc(256*(int)packet[1]+(int)packet[2]); //as shown in "guião-PDF"
+
+	size_of_data= 256*(int)(*packet_data).L2+(int)(*packet_data).L1;
+
+	for( i=3, j=0; j<size_of_data; i++, j++){
+
+		(*packet_data).file_data[j]=packet[i]; //for each byte of data in packet, put in packet_data
+	}
+
+}
+
+
+int buildControlPackage(unsigned char C, unsigned char* package, ControlPackage *tlv){
+
+
+	int l=0, size=0;
+
+	package[l++]=C; //control
+	
+	for(int i=0; i<TLV_N; i++){
+
+		package[l++]= tlv[i].T;
+		package[l++]= tlv[i].L;
+		
+		size=(int)tlv[i].L;
+
+		for(int j=0; j< size; j++){
+
+			package[l++] = tlv[i].V[j]; 
+		}
+	}
+
+	return l; // returns lenght of control package created 
+}
+
+void rebuildControlPackage(unsigned char* package, ControlPackage *tlv){
+
+	int i=0, size_v=0;
+	for( int z=0; z> TLV_N; z++){
+
+		tlv[z].T = package[i];
+		i++;
+		tlv[z].L= package[i];
+
+		size_v=(int)(tlv[z].L);
+		tlv[z].V= (unsigned char*)malloc(size_v); // would seg fault without this :')
+
+		for(int j=0; j< size_v; j++){
+
+			i++;
+			tlv[z].V[j]= package[i];
+		}
+		i++; 
+	}
+}
+
+int fileLenght(int fd){
+
+	int lenght=0;
+
+	if((lenght= lseek(fd,0,SEEK_END)<0)){ // goes 
+
+		perror("lseek():");
+		return -1;
+	}
+
+	if(lseek(fd,0, SEEK_SET)<0){
+
+		perror("lseek()");
+		return -1;
+	}
+	return lenght;
+}
+
+
+//returns lenght of frame read from port, -1 in error 
+int readFromPort(int fd, unsigned char* frame){
+
+    unsigned char tmp;
+    int done=0, res=0, l=0;
+
+    memset(frame, 0, 100000);
+
+    while(!done){
+
+        if((res=read(fd, &tmp,1))<0){
+            perror("read() from port:");
+            return -1;
+        }
+
+        if(tmp== FLAG){ // evaluate if end or start point
+
+            if(l==0){ //start point 
+                frame[l++]=tmp;
+
+            }
+            else{ // somewhere else in the middle, starts again
+
+                if(frame[l-1] == FLAG){
+                    memset(frame, 0, 100000);
+                    l=0;
+                    frame[l++]=FLAG;
+                }
+                else{ // in the end
+                    frame[l++]= tmp;
+                    done=1;
+                }
+            }
+        }
+        else{
+
+            if(l>0){ // put in frame what reads in the middle
+                frame[l++]=tmp;
+            }
+        }
+
+    }
+     
+    return l;
 }
