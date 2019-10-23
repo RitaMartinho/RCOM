4b9878b959c1af45966c05da62b961b27ba05984
GoncaloXavier <goncalox11@gmail.com>
2019-10-23 19:27:43 +0100

    Tried to make a noise generator

--- tools.c
+++ tools.c
@@ -136,10 +136,14 @@ int buildDataPackage(unsigned char* buffer, unsigned char* package, int size, in
 	aux=size%256;
 	package[2]=(size -aux)/256;
 	package[3]=aux;
-
+	
+	srand(time(0));
+	int random=rand()%10;
 	for(i=0; i<size; i++){
-
-		package[i+4]=buffer[i]; // data read from file into application package
+		if(random==2){
+			package[i+4]=0;
+		}
+		else package[i+4]=buffer[i]; // data read from file into application package
 	}
 
 	return i+4; // returns size of package
